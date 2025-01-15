906f2cbe3664e4da2ab2e915db4f8043e1c0e657
rahul <kotterhl@gmail.com>
2025-01-15 14:48:06 +0530

    commited

---
+++ src/main/java/com/app/demo/Sem_maven_java/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Sem_maven_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Kotte Rahul" );
+    }
+}
